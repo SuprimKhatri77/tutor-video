edb5f06bf99de8ce0b81950fb883ba31a39f8096
roshan393 <chanantony29@gmail.com>
2026-01-09 15:35:55 +0545

    responsive upto 300px

--- components/landing/About.tsx
+++ components/landing/About.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 
 export const About = () => {
   return (
-    <section id="about" className="py-16 px-4 sm:px-6 lg:px-8 bg-white">
+    <section className="py-16 px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
-        <div className="bg-linear-to-br from-gray-50 to-blue-50 rounded-3xl p-8 md:p-12 shadow-lg">
+        <div className="bg-linear-to-br from-gray-50 to-blue-50 rounded-3xl p-4 sm:p-8 md:p-12 shadow-lg">
           <div className="grid lg:grid-cols-2 gap-12 items-center">
             {/* Left - Profile Image */}
             <div className="flex justify-center lg:justify-start">
               <div className="relative">
-                <div className="w-72 h-72 rounded-full overflow-hidden border-8 border-white shadow-2xl relative">
+                <div className="w-60 sm:w-72 h-60 sm:h-72 rounded-full overflow-hidden border-8 border-white shadow-2xl relative">
                   <div className="absolute inset-0 bg-linear-to-b from-black via-red-600 to-yellow-400"></div>
                   <Image
                     src="https://p19-common-sign-useastred.tiktokcdn-eu.com/tos-useast2a-avt-0068-euttp/5d7431fe4cbc45dadde899954e434e7f~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=b45fa506&x-expires=1767873600&x-signature=oVZ6D6oiYbCt95JRVFS7mn%2BckCw%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my"
@@ -30,10 +30,10 @@ export const About = () => {
             {/* Right - Content */}
             <div className="space-y-6">
               <div>
-                <h2 className="text-4xl md:text-5xl font-bold text-gray-900 mb-2">
+                <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold text-gray-900 mb-2">
                   Hello! I&apos;m  Vikas
                 </h2>
-                <p className="text-blue-600 font-semibold text-lg">
+                <p className="text-blue-600 font-semibold sm:text-lg">
                   C1 Certified German Tutor
                 </p>
               </div>
